144d56138325fff9c94d8be0532468ec7c1d8ccb
Matyas <matyas.szabo@protonmail.com>
2022-11-30 18:23:43 +0100

    comment unused class for now

--- src/main/java/com/codecool/fileshare/repository/ImageFileRepository.java
+++ src/main/java/com/codecool/fileshare/repository/ImageFileRepository.java
@@ -2,9 +2,9 @@ package com.codecool.fileshare.repository;
 
 import org.springframework.stereotype.Component;
 
-@Component("file")
+/*@Component("file")
 public class ImageFileRepository implements ImageRepository{
-    //Recomenden implementation. store data about users and images in csv files.
+    //Recommended implementation. store data about users and images in csv files.
     //store the images in the same folder with image name like 41d6608d-0803-4239-9235-09f902fbf705.jpg
 
-}
+}*/
