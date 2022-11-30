59c3e62a0007eb7e5e3f3e5f369a50f1622ec10e
kalapisd <kalapisd@gmail.com>
2022-11-30 21:56:40 +0100

    add new ImageAlreadyInDatabaseException

---
+++ src/main/java/com/codecool/fileshare/exception/ImageAlreadyInDatabaseException.java
@@ -0,0 +1,8 @@
+package com.codecool.fileshare.exception;
+
+public class ImageAlreadyInDatabaseException extends RuntimeException {
+    public ImageAlreadyInDatabaseException(String message) {
+        super(message);
+    }
+
+}
