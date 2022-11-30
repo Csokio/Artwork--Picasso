6194bd71eb2842dc53185834a64ad1ad28612a82
kalapisd <kalapisd@gmail.com>
2022-11-30 22:48:17 +0100

    add fields to image class

--- src/main/java/com/codecool/fileshare/model/Image.java
+++ src/main/java/com/codecool/fileshare/model/Image.java
@@ -1,6 +1,19 @@
 package com.codecool.fileshare.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
 public class Image {
+
+    private String id;
+    private String title;
+    private String description;
+    private String extension;
 
 }
